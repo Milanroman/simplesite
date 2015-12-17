dc99f429e4d82750cbac4d11861580ff8197a649
romkushka <romkushka@gmail.com>
2015-12-17 14:23:42 +0000

    added table

--- scripts/weatherByCity.js
+++ scripts/weatherByCity.js
@@ -1,10 +1,10 @@
 function getWeatherByCity(lang, fnOK, fnError, cityName) {
     $.getJSON(
         'http://api.openweathermap.org/data/2.5/forecast?q=' 
-        + cityName + '&APPID=19e6212ba1a15130d764c8ce642a543b&cnt=16&units=metric'
+        + cityName + '&APPID=19e6212ba1a15130d764c8ce642a543b&cnt=8&units=metric'
         + '&lang=' + lang + '&callback=?',
         function (data) {
             fnOK.call(this,data);
         }
-        );
+    );
 }
--- scripts/weatherService.js
+++ scripts/weatherService.js
@@ -1,38 +1,46 @@
-$(function() {
-            navigator.geolocation.getCurrentPosition(locSuccess, locError);
-        });
+function getWeatherData (lang, fnOK, fnError) {
+    navigator.geolocation.getCurrentPosition(locSuccess, locError);
 
     function locSuccess(position) {
-        var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + position.coords.latitude +
-                                             '&lon=' + position.coords.longitude + '&APPID=19e6212ba1a15130d764c8ce642a543b&units=metric' + '&callback=?';
-        $.getJSON(weatherAPI, function (response) {
-            $('#city').html(response.city.name + ', ' + response.city.country);
-            $('#time_now0').html(response.list[0].dt_txt);
-            $('#time_now1').html(response.list[1].dt_txt);
-            $('#time_now2').html(response.list[2].dt_txt);
-            $('#time_now3').html(response.list[3].dt_txt);
-            $('#time_now4').html(response.list[4].dt_txt);
-            $('#temp_now0').html(Math.round(response.list[0].main.temp) + '&deg;C');
-            $('#temp_now1').html(Math.round(response.list[1].main.temp) + '&deg;C');
-            $('#temp_now2').html(Math.round(response.list[2].main.temp) + '&deg;C');
-            $('#temp_now3').html(Math.round(response.list[3].main.temp) + '&deg;C');
-            $('#temp_now4').html(Math.round(response.list[4].main.temp) + '&deg;C');
-            $('#humidity0').html(response.list[0].main.humidity);
-            $('#humidity1').html(response.list[1].main.humidity);
-            $('#humidity2').html(response.list[2].main.humidity);
-            $('#humidity3').html(response.list[3].main.humidity);
-            $('#humidity4').html(response.list[4].main.humidity);
-            $('#wind0').html(response.list[0].wind.speed);
-            $('#wind1').html(response.list[1].wind.speed);
-            $('#wind2').html(response.list[2].wind.speed);
-            $('#wind3').html(response.list[3].wind.speed);
-            $('#wind4').html(response.list[4].wind.speed);
-        });
+        // Check cache
+        var cache = localStorage['weatherCache'] && JSON.parse(localStorage['weatherCache']);
+        var currDate = new Date();
+        // If the cache is newer than 5 minutes, use the cache
+        if (cache && cache.timestamp && cache.timestamp > currDate.getTime() - 5*60*1000){
+            fnOK.call(this, cache.data);
+        } else {
+            $.getJSON(
+                'http://api.openweathermap.org/data/2.5/forecast?lat=' + position.coords.latitude + '&lon=' +
+                position.coords.longitude + '&cnt=8&units=metric&APPID=19e6212ba1a15130d764c8ce642a543b' + '&lang=' + lang + '&callback=?',
+                function (response) {
+                    // Store the cache
+                    localStorage.weatherCache = JSON.stringify({
+                        timestamp: (new Date()).getTime(),	// getTime() returns milliseconds
+                        data: response
+                    });
+                    // Call the function again
+                    locSuccess(position);
+                }
+            );
+        }
     }
 
     function locError(error) {
-        console.warn('Error:' + error.message);
+        var message = 'Location error. ';
+        switch(error.code) {
+            case error.TIMEOUT:
+                message += 'A timeout occured! Please try again!';
+                break;
+            case error.POSITION_UNAVAILABLE:
+                message += 'We can\'t detect your location. Sorry!';
+                break;
+            case error.PERMISSION_DENIED:
+                message += 'Please allow geolocation access for this to work.';
+                break;
+            case error.UNKNOWN_ERROR:
+                message += 'An unknown error occured!';
+                break;
+        }
+        fnError.call(this, message);
     }
-    
-
-
+}
